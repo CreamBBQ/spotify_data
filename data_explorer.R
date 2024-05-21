72bd5c55d35980cebbaa113f48d3743bf519588d
Guillermo Muñoz <jgmd1306@gmail.com>
2024-05-21 12:06:43 -0300

    Conexiones con la API - top artist y top tracks

--- data_explorer.R
+++ data_explorer.R
@@ -5,34 +5,29 @@ Sys.setenv(SPOTIFY_CLIENT_ID =
              readLines("C:/Users/guill/Documents/projects/credentials/spotify.txt")[2])
 Sys.setenv(SPOTIFY_CLIENT_SECRET = 
              readLines("C:/Users/guill/Documents/projects/credentials/spotify.txt")[5])
-access_token <- get_spotify_access_token()
 
+tracks <- get_my_top_artists_or_tracks(
+  type = "tracks",
+  limit = 50,
+  offset = 0,
+  time_range = "short_term",
+  authorization = get_spotify_authorization_code(scope = "user-top-read"),
+  include_meta_info = FALSE
+)
+
+artist_top <- get_my_top_artists_or_tracks(
+  type = "artists",
+  limit = 50,
+  offset = 0,
+  time_range = "short_term",
+  authorization = get_spotify_authorization_code(scope = "user-top-read"),
+  include_meta_info = FALSE
+)
+
+
+################################################################################
 ye <- get_artist_audio_features("kanye west")
-
-deva <- get_artist_audio_features("DEVA")
-
 ye %>% 
   filter(album_name == "ye") %>% 
   select(artist_name, album_name, album_release_date, track_name, 
          danceability, energy, loudness, speechiness, acousticness, valence)
-
-ye %>% 
-  arrange(valence) %>% 
-  select(artist_name, album_name, album_release_date, track_name, 
-         danceability, energy, loudness, speechiness, acousticness, valence) %>% 
-  head(10)
-  
-bmth <- get_artist_audio_features("Bring Me The Horizon") 
-bmth %>% arrange(-danceability) %>% 
-  filter(album_name == "amo") %>% 
-  select(artist_name, album_name, track_name, danceability, valence) %>% 
-  head(20)
-
-
-ysy <- get_artist_audio_features("YSY A") 
-ysy %>% arrange(-valence) %>% 
-  select(artist_name, album_name, track_name, danceability, valence) %>% 
-  head(20)
-
-
-################################################################################
