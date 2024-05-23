3a3cc16e7f02a9e74a0c31a5fbec8c53778d8f0d
Guillermo Muñoz <jgmd1306@gmail.com>
2024-05-23 11:25:38 -0300

    top songs finished

--- data_explorer.R
+++ data_explorer.R
@@ -6,14 +6,47 @@ Sys.setenv(SPOTIFY_CLIENT_ID =
 Sys.setenv(SPOTIFY_CLIENT_SECRET = 
              readLines("C:/Users/guill/Documents/projects/credentials/spotify.txt")[5])
 
+fecha_yyyymm <- "202405"
+
+################################# TOP SONGS ####################################
+
 tracks <- get_my_top_artists_or_tracks(
   type = "tracks",
-  limit = 50,
+  limit = 10,
   offset = 0,
   time_range = "short_term",
   authorization = get_spotify_authorization_code(scope = "user-top-read"),
   include_meta_info = FALSE
 )
+
+process_element <- function(element) {
+  names <- element$name
+  if(length(names) > 1) {
+    paste(names, collapse = " - ")
+  } else {
+    names
+  }
+}
+
+tracks <- tracks %>% 
+  select(id, name, artists) %>% 
+  mutate(rank = seq(1:10), 
+         artists = sapply(artists, process_element), 
+         date = fecha_yyyymm)
+
+tracks <- tracks %>% 
+  merge(., 
+        get_track_audio_features(.[["id"]], authorization = get_spotify_access_token()) %>% 
+          select(acousticness, danceability, energy, valence, id)) %>% 
+  arrange(., rank)
+
+
+################################# TOP SONGS ####################################
+
+
+
+########################################################################################
+
 
 artist_top <- get_my_top_artists_or_tracks(
   type = "artists",
